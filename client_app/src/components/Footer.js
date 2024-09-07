1b8b1d6681e172f7310b903ec24427dd80080284
Sherry <126199512+hussaiinkhan@users.noreply.github.com>
2024-09-07 17:29:37 +0300

    Add files via upload

---
+++ client_app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <p className='font-semibold text-sm text-center mt-4 mb-2'>Created by Hussain Mehboob using MERN Stack.</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ client_app/src/components/PlacePage.js
@@ -0,0 +1,53 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+import PlaceGallery from "./PlaceGallery";
+import AddressLink from "./AddressLink";
+import BookingWidget from "./BookingWidget";
+
+export default function PlacePage() {
+  const {id} = useParams();
+  const [place,setPlace] = useState(null);
+  const pricePerNight = (Math.random() * 30).toFixed(2)
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    axios.get(`http://localhost:5001/api/places/${id}`).then(response => {
+      setPlace(response.data);
+    });
+  }, [id]);
+
+  if (!place) return '';
+
+
+
+  return (
+    <div className="mt-4 bg-gray-100  px-8 sm:px-20 pt-8">
+      <h1 className="text-3xl">{place.title}</h1>
+      <AddressLink>{place.address}</AddressLink>
+      <PlaceGallery place={place} />
+      <div className="mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
+        <div>
+          <div className="my-4">
+            <h2 className="font-semibold text-2xl">Description</h2>
+            {place.description}
+          </div>
+          Check-in: {place.checkIn}<br />
+          Check-out: {place.checkOut}<br />
+          Max number of guests: {place.maxGuests}
+        </div>
+        <div>
+          <BookingWidget place={place} pricePerNight={pricePerNight}/>
+        </div>
+      </div>
+      <div className="   py-8 border-t">
+        <div>
+          <h2 className="font-semibold text-2xl">Extra info</h2>
+        </div>
+        <div className="mb-4 mt-2 text-sm text-gray-700 leading-5">{place.extraInfo}</div>
+      </div>
+    </div>
+  );
+}
